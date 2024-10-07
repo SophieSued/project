16005e033854553d0062c857379aa2b656c009d6
SophieSued <163852514+SophieSued@users.noreply.github.com>
2024-10-07 08:44:34 -0300

    Environment

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
-import fastapi 
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
 import os
-import psycopg2
-import uvicorn
-from typing import Annotated
-from fastapi import File, UploadFile
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import JSONResponse
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+
 
 app = FastAPI()
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    print(file)
-    return {"filename": file.filename}
+# Cargar el modelo entrenado
+MODEL_PATH = "trained_model_2.keras"
+# MODEL_PATH = "model.weights.h5"
+model = load_model(MODEL_PATH)
 
-# Define el directorio donde se guardará el archivo
-UPLOAD_FOLDER = "C:\\Users\\48240152\\Downloads\\modelo"
+# Definir el directorio donde se guardará el archivo
+UPLOAD_FOLDER = "uploads" # es la carpeta de uploads en visual
+
+# Definir las clases (0 -> Sano, 1 -> Infectado)
+CLASSES = ["Sano", "Infectado"]
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     try:
         # Crear la ruta completa del archivo
-        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        # file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
         # Guardar el archivo en el sistema
-        with open(file_path, "wb") as f:
-            f.write(await file.read())  # `await` es necesario para manejar la lectura asincrónica del archivo
-        
-        # Responder con éxito
-        return {"message": "File saved successfully", "file_path": file_path}
+        # with open(file_path, "wb") as f:
+        #   f.write(await file.read()) 
+
+        # Cargar y preprocesar la imagen para el modelo
+        img = Image.open(file.file).resize((100, 100))  
+        img_array = np.array(img) / 255.0  
+        img_array = np.expand_dims(img_array, axis=0)  
+        # Realizar la predicción
+        prediction = model.predict(img_array)
+        predicted_class = np.argmax(prediction, axis=1)[0]  
+
+        # Traducir la clase en 'sano' o 'infectado'
+        result = CLASSES[predicted_class]
+
+        # Responder con la predicción
+        return {"message": "File saved successfully", "prediction": result}
     
     except Exception as e:
-        # Manejo de errores
-        return {"message": f"Error saving file: {str(e)}"}
+        return JSONResponse(status_code=500, content={"message": f"Error processing file: {str(e)}"})
